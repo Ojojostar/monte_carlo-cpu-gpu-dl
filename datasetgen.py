3cf3c3f5bd2b0d1f2ed1809484c68332c9ab95f5
Ojojostar <josephtang26@gmail.com>
2024-07-04 17:01:27 +0800

    made generating data easier

--- datasetgen.py
+++ datasetgen.py
@@ -9,10 +9,11 @@ from torch.utils.dlpack import from_dlpack
 import numba
 from numba import cuda
 
+import os, os.path
+from pathlib import Path
 
-
-
-
+# code was written with batch of size 1 in mind and number of data points gets a bit off the inputed amount
+# if the division ends in a decimal
 
 
 @cuda.jit               # defualt GPU
@@ -25,7 +26,6 @@ def monte_carlo_andtheholygrail_gpu(d_s, s_0, Ki, Ko, mu, sigma, pot,r,
     # shared = cuda.shared.array(shape=0, dtype=numba.float32)
     # # load to shared memory
     # path_offset = cuda.blockIdx.x * cuda.blockDim.x
-
     # ii - overall thread index
     ii = cuda.threadIdx.x + cuda.blockIdx.x * cuda.blockDim.x
     stride = cuda.gridDim.x * cuda.blockDim.x
@@ -34,7 +34,7 @@ def monte_carlo_andtheholygrail_gpu(d_s, s_0, Ki, Ko, mu, sigma, pot,r,
         # newly added vars for N_BATCH calculations
         batch_id = n // N_PATHS
         path_id = n % N_PATHS       # equivalent to n in old code 
-
+                    
         snowball_path_holder[n][0] = s_0[batch_id]
         earlyexit = False
         ki = False
@@ -75,7 +75,8 @@ def monte_carlo_andtheholygrail_gpu(d_s, s_0, Ki, Ko, mu, sigma, pot,r,
                             mald = True
                             # print("blo got fucked\n")
                             break
-
+            else: # if mald
+                break
         
         if not earlyexit:       # to prevent early exit getting out of bdds error
             # did not get knocked up or down
@@ -90,43 +91,55 @@ def monte_carlo_andtheholygrail_gpu(d_s, s_0, Ki, Ko, mu, sigma, pot,r,
             d_s[n] = price * math.exp(-r[batch_id])
             snowball_path_holder[n][-1] = d_s[n]    
 
-
+#               make sure max_len is large enough or else divide by zero error occurs (at least 100 batches must be run)
+limiter = True
 max_len = 1000000                 # hundo thousand data points, final speed is ~40 min for 1000 data.
+# max_len = 100                 
 number_path = 500000
 batch = 1
-threads = 256
+threads = 512
 seed  =1999 
 num = 0
 max_length = max_len
 N_PATHS = number_path
 N_STEPS = 365
 N_BATCH  =batch
-# we will not be calculating a starting date since the difference is negligible and I aint rigging up
-# a system to check if a certain day is a weekend or not
+max_length = max_length // N_BATCH
+percenter  =100       #  changes how often percent text gets shown & how many times things are saved
+percent = max_length // percenter
+
+#           uncomment if u want less batches, the percent will just be wrong
+if percent == 0:
+    percent = 1
+
+
 MONTHS = cupy.asnumpy([0, 31,59,90,120,151,181,212,243, 273,304,334])
-        # SHOULD THIS BE NP ARRAY INSTEAD????
-snowball_path_holder =  np.zeros(N_BATCH*N_PATHS, dtype=(np.float32,N_STEPS+1))# extra 1 is no longer for storing payoff
-# self.snowball_path_holder = cupy.array(self.snowball_path_holder)
-# self.T  = np.float(365.0)         # nah id lose. 
+snowball_path_holder =  np.zeros(N_BATCH*N_PATHS, dtype=(np.float32,N_STEPS+1))
 output = cupy.zeros(N_BATCH*N_PATHS, dtype = cupy.float32)
 num_blocks  =(N_PATHS * N_BATCH -1) // threads +1
 num_threads = threads
 
 
-# Xs =  np.zeros(max_length, dtype=(np.float32,7))#
-# Ys =  np.zeros(max_length, dtype=(np.float32))#          storing final data
-
 Xss = []
 Yss = []
-# Xss = np.zeros((max_length, 7))
-# Yss = np.zeros((max_length, 1))
+path = Path(__file__).parent.absolute()
+folder = "snow_data_tensor_train"
+dir = f"{path}\{folder}" 
+
+print("\nPRINGITN CURRENT DIR", dir)
+currnum = len(os.listdir(dir))//2+1
+print("Batch size:", N_BATCH)
+print("Adding files starting from", currnum, "\n")
 
 
-# making sure self.snowball_path_holder is zeroed to avoid bug
-# self.snowball_path_holder.fill(0)
+
+start = 1
+if limiter:
+    start = currnum
+
 s = time.time()
 
-for i in range(max_length):
+for i in range(start,max_length+1):
         randoms = cupy.random.normal(0,1, N_BATCH * N_PATHS * N_STEPS, dtype= cupy.float32)
 
         Xpre = cupy.random.rand(N_BATCH, 7, dtype = cupy.float32)
@@ -136,7 +149,6 @@ for i in range(max_length):
         # Ki and Ko will be set down here instead of the previous line to make them relative to s_0.
         X[:, 1] = X[:,0] -1         # overriding Ki and Ko 
         X[:, 2] = X[:,0] -.2        
-        # print(X)
         X[:, 1] += Xpre[:,1]        # adding back the offset in Xpre after it gets overrided
         X[:, 2] += Xpre[:,2] 
 
@@ -151,6 +163,8 @@ for i in range(max_length):
                                         N_STEPS, N_PATHS, N_BATCH)
         # o = output.reshape(N_BATCH, N_PATHS)
         # Y  =o.mean(axis =1)         # getting the average of each batch
+        cuda.synchronize()
+        # cupy.cuda.stream.get_current_stream().synchronize()
         Y = output.mean()
         X = X.mean(axis=0)
         Xss.append(X.tolist())
@@ -160,9 +174,69 @@ for i in range(max_length):
         
         # torch.save(from_dlpack(X.toDlpack()), f"snow_data/snowX_data/tensor{i}.pt")
         # torch.save(from_dlpack(Y.toDlpack()), f"snow_data/snowY_data/tensor{i}.pt")
-        if(i%100000==0):
+
+        # if(i%i==0):                   # for testing purposed only!!
+        if(i%percent==0):
+            if limiter:
+                if currnum > percenter:
+                    print("\nPremature exit, burunyu~")
+                    print(" ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢭⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                          "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣶⣝⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣾⣿⣿⣿⣿⣿⡿⣫⣶⣿⣿⣿⢸⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣮⢿⣿⣝⣿⣿⡿⢟⣛⣭⣭⣝⣿⡏⢹⣿⣿⣻⣾⣿⢟⣿⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡻⣼⣿⣟⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣯⢿⣿⣿⡇⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣿⣿⣿⣿⣿⣿⣿⡇⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣷⠻⣿⣿⣿⣻⣾⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⣷⣿⣿⣿⣻⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢇⣿⣿⣿⣿⣿⣿⣿⣿⣇⣿⣿⣿⣿⣿⣿⣷⣿⣿⡼⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⢻⣿⣿⣿⣿⣿⡏⣿⣿⣿⣸⣿⣿⣿⣿⣿⣯⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⡾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢹⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣏⣿⣿⣿⣿⣿⣿⢻⣿⣻⣿⢸⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣇⣿⣼⣿⣿⣿⠀⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣽⣿⣿⣿⡇⣿⣛⡍⠀⣿⣿⣶⣼⢿⣿⣿⡿⣿⣻⣿⠸⣛⣿⣼⣻⣃⠀⣿⣿⣿⣿⣿⡞⣿⣿\n",
+                            "⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢳⣿⣿⣿⠀⠀⠀⣿⣿⣿⣿⣿⡽⣿⣟⣿⠀⠀⢸⣿⣿⣿⣿⣿⢻⣿⣿⣿⣼⣿⣿⣿\n",
+                            "⣿⣿⣏⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⢸⣿⣿⣿⣿⣿⡇⣿⣿⣿⡇⣿⡇⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡼⣿⣿⣿⠀⠀⢠⣿⣿⣿⣿⣿⣳⣿⣻⣿⠀⠀⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⡇⣻⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⣿⡟⣿⣿⣻⣿⣤⣿⣿⣿⢟⣿⣿⣿⣿⣿⣿⣷⣻⣿⣟⣯⣾⣿⢷⣿⣿⣿⣿⣿⣿⣿⣅\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⢿⣿⣿⣿⠀⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⣿⣿⣿⣿⢸⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣧⣿⣿⡇⠀⠀⣿⣿⡏⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⢸⣿⣝⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⣿⣿⣿⣿⡆⠀⠀⢰⡀⠀⠀⠙⠻⠿⣿⣿⣿⡿⠟⠋⠁⠀⠀⣿⠀⠀⠀⣿⣿⣿⣿⣷⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣴⣿⣿⣿⣿⣿⣿⣿⣺⣿⣮⣦⠀⢸⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⡿⣵⣿⣮⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⢹⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣳⣿⣿⣿⣿⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⢸⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢻⣝⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢘⣶⣶⣯⢿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣻⣿⣿⣿⣿⣝⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⢇⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣯⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣯⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠉⠉⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣝⠻⠿⣛⣾⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⠟⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⣶⣄⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⣼⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣷⣄⣠⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n",
+                            "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿\n")
+                    # Xss.pop()
+                    # Yss.pop()
+                    break
             e = time.time()
-            print(i//10000, "percent of the way there! Time is now:", (e-s)/60/60, "hours")
+            print(i/(percent), "percent of the way there! Time is now:", (e-s)/60/60, "hours")
+            # print(i/(percent), "percent of the way there! Time is now:", e-s, "secs")
+            print("now saving tsnowX_{}.pt".format(currnum) )
+            tensorX = np.array(Xss)
+            tensorY = np.array(Yss)
+            tensorX = torch.Tensor(tensorX)
+            tensorY = torch.Tensor(tensorY)
+            torch.save(tensorX, f"{dir}\\tsnowX_{currnum}.pt")
+            torch.save(tensorY, f"{dir}\\tsnowY_{currnum}.pt")
+            Xss.clear()
+            Yss.clear()
+            currnum += 1
         num+=1
         # print((from_dlpack(X.toDlpack()), from_dlpack(Y.toDlpack()))) 
 
@@ -170,20 +244,15 @@ v = output.mean()
 cuda.synchronize()
 e = time.time()
 print("done!!! Yuri!!! :yuristar:")
-print('time', e-s, 'v', v, 'avg time', (e-s)/500000)
+print('time', e-s, 'v', v)
 
-# Xs = np.array(Xss)
-# Ys = np.array(Yss)
-Xss = np.array(Xss)
-Yss = np.array(Yss)
-# print(Xss)
-# print(Yss)
 
-tensorX = torch.Tensor(Xss)
-tensorY = torch.Tensor(Yss)
-# print(tensorX)
-# print(tensorY)
-torch.save(tensorX, "snow_data_tensor_train/tsnowX.pt")
-torch.save(tensorY, "snow_data_tensor_train/tsnowY.pt")
-# np.save("snow_npy_data/npyX", Xss)
-# np.save("snow_npy_data/npyY", Yss)
+#           may need to use if percent doesn't end on 100
+# if Xss:                     # chekcing if its empty of not. only pritn if isnt.
+#     print("this seems odd...", len(Xss))
+#     Xss = np.array(Xss)
+#     Yss = np.array(Yss)
+#     tensorX = torch.Tensor(Xss)
+#     tensorY = torch.Tensor(Yss)
+#     torch.save(tensorX, "snow_data_tensor_train/tsnowX.pt")
+#     torch.save(tensorY, "snow_data_tensor_train/tsnowY.pt")
